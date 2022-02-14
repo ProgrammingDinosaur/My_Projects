13b3407f70f46f5ec3ea452f7789dbb8f906cdf7
Andres Hernandez <76182058+ProgrammingDinosaur@users.noreply.github.com>
2022-02-14 14:34:43 -0600

    Add files via upload

--- Statistics/utils_estadistica.py
+++ Statistics/utils_estadistica.py
@@ -1,17 +1,17 @@
-#Import Pandas and Excel library for DataFrame and Workbook manipulation
-
-from re import A
 import pandas as pd
 import numpy as np
 from scipy.stats.mstats import gmean
 import statistics
 
 class InvalidDim(Exception):
+    '''
+    Raises Exception when given an array with more than 1-Dimension
+    '''
     pass
 
 
 ##Tabla Disttribucion de Frec.##
-def crear_tabla_integros(dataframe: pd.DataFrame,index = 1):
+def crear_tabla_integros(dataframe: pd.DataFrame,index = 1)->pd.DataFrame:
     '''
     Crea una tabla de distribucion de frecuencias dado un indice de columna y un Dataframe de Pandas, las clases se dividen por cada valor unico
         
@@ -43,9 +43,19 @@ def crear_tabla_integros(dataframe: pd.DataFrame,index = 1):
 
     return table
     
-def crear_tabla_por_intervalos(dataframe: pd.DataFrame,start_val:int,end_val: int, intervalo: int,index = 1):
+def crear_tabla_por_intervalos(dataframe: pd.DataFrame,start_val:int,end_val: int, intervalo: int,index = 1)->pd.DataFrame:
     '''
     Crea una tabla de distribucion de frecuencias con rango e intervalos predefinidos
+    Parametros:
+
+            dataframe: Dataframe de Pandas para realizar tabla, valor default = None
+            start_val: Valor donde iniciar las clases
+            end_val:  Valor donde terminar las clases
+            intervalo: Valor por el que deben ir separadas las clases
+            index: indice de la tabla con los datos, valor default = 1
+
+        Regresa:
+            table: Pandas Dataframe
     '''
     table = pd.DataFrame()
     frec_abs = []
@@ -74,25 +84,24 @@ def crear_tabla_por_intervalos(dataframe: pd.DataFrame,start_val:int,end_val: in
 
     return table
     
-
 ##Medidas Estadistica##
-def media_arit(dataframe: pd.DataFrame, index = 1):
+def media_arit(dataframe: pd.DataFrame, index = 1)->np.ndarray:
     return np.mean(dataframe.iloc[:,index].values)
 
-def media_geometrica(dataframe: pd.DataFrame, index = 1):
+def media_geometrica(dataframe: pd.DataFrame, index = 1)->np.ndarray:
     return gmean(dataframe.iloc[:,index].values)
 
-def mediana(dataframe: pd.DataFrame,index = 1):
+def mediana(dataframe: pd.DataFrame,index = 1)->np.ndarray:
     return np.median(dataframe.iloc[:,index].values)
 
-def moda(dataframe: pd.DataFrame,index = 1):
+def moda(dataframe: pd.DataFrame,index = 1)->np.ndarray:
     moda_list = list(dataframe.iloc[:,index].values)
     return statistics.mode(moda_list)
 
-def media_ponderada(dataframe: pd.DataFrame,valores_idx = 1, pesos_idx = 2):
+def media_ponderada(dataframe: pd.DataFrame,valores_idx = 1, pesos_idx = 2)->np.ndarray:
     return np.average(dataframe.iloc[:,valores_idx].values,weights=dataframe.iloc[:,pesos_idx].values)
 
-def calcular_tasa(rango_valores: np.ndarray,media_tipo = 'arit'):
+def calcular_tasa(rango_valores: np.ndarray,media_tipo = 'arit')->np.ndarray:
     media_tipos = ['arit','gmean']
     if ((len(rango_valores.shape)) > 1):
         raise InvalidDim("The passed ndarray should be 1-dimensional")
@@ -105,10 +114,23 @@ def calcular_tasa(rango_valores: np.ndarray,media_tipo = 'arit'):
     value_hist = np.array(value_hist)
     return np.mean(value_hist) if media_tipo == 'arit' else gmean(value_hist)
 
-def media_agrupada(valores: np.ndarray, intervalo: int):
-    return statistics.median_grouped(valores,intervalo)
+def media_agrupada(tabla_dist: np.ndarray, frec_abs_idx = 1)->float:
+    values = tabla_dist.iloc[:,frec_abs_idx].values
+    total_frec_abs = np.sum(values)
+    count = 0
+    for idx,value in enumerate(values): 
+        count += value
+        if count > total_frec_abs:
+            median_index = idx
+            break
+    rango = tabla_dist.iloc[median_index,0].split('-')
+    first_arg = rango[0]
+    second_arg = rango[1]
+    a = second_arg-first_arg
+    mediana_agrup = first_arg+((((np.sum(values))/2)-values[median_index-1])/(values[median_index]))*a
+    return mediana_agrup
 
-def moda_agrupada_mismos_intervalos(tabla_dist: pd.DataFrame,frec_abs_idx = 1):
+def moda_agrupada_mismos_intervalos(tabla_dist: pd.DataFrame,frec_abs_idx = 1)->float:
     values = tabla_dist.iloc[:,frec_abs_idx].values
     max_val = np.argmax(values)[0]
     rango = tabla_dist.iloc[max_val,0].split('-')
